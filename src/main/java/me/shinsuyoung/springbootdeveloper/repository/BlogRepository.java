8811eea67891b9982a735e8c345f6d2bff2aadd8
Hailey <116434954+hailey88888@users.noreply.github.com>
2023-07-31 17:55:27 +0900

    Delete src directory

--- src/main/java/me/shinsuyoung/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package me.shinsuyoung.springbootdeveloper.repository;
-
-import me.shinsuyoung.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-
-    // JpaRepository 클래스를 상속 받을때 엔티티 Article과 엔티티의 PK타입 longdf dlstnfh sjgdma
-}
